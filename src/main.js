ed3037a29f072f2e9ada8a5b504694a824e2ddf4
yanhan-maker <424133659@qq.com>
2021-05-10 09:00:00 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App  from "./App";
+//引用router.js
+import router from './router'
+Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+//导入axios
+import Axios from "axios";
+//配置异步将要访问的路径
+Axios.defaults.baseURL="http://127.0.0.1:8090/code"
+
+//将baseURL绑定到全局
+Vue.prototype.$axios = Axios
+
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
